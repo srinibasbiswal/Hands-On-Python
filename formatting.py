c9c21ed45950f1b73e33e38b7467f2a9a290b215
Srinibas Biswal <srinibasbiswal12@gmail.com>
2019-05-19 20:48:11 +0530

    Add format() file

---
+++ formatting.py
@@ -0,0 +1,58 @@
+# default arguments
+print("Hello {}, your balance is {}.".format("Adam", 230.2346))
+
+# positional arguments
+print("Hello {0}, your balance is {1}.".format("Adam", 230.2346))
+
+# keyword arguments
+print("Hello {name}, your balance is {blc}.".format(name="Adam", blc=230.2346))
+
+# mixed arguments
+print("Hello {0}, your balance is {blc}.".format("Adam", blc=230.2346))
+
+# integer arguments
+print("The number is:{:d}".format(123))
+
+# float arguments
+print("The float number is:{:f}".format(123.4567898))
+
+# octal, binary and hexadecimal format
+print("bin: {0:b}, oct: {0:o}, hex: {0:x}".format(12))
+
+# integer numbers with minimum width
+print("{:5d}".format(12))
+
+# width doesn't work for numbers longer than padding
+print("{:2d}".format(1234))
+
+# padding for float numbers
+print("{:8.3f}".format(12.2346))
+
+# integer numbers with minimum width filled with zeros
+print("{:05d}".format(12))
+
+# padding for float numbers filled with zeros
+print("{:08.3f}".format(12.2346))
+
+# show the + sign
+print("{:+f} {:+f}".format(12.23, -12.23))
+
+# show the - sign only
+print("{:-f} {:-f}".format(12.23, -12.23))
+
+# show space for + sign
+print("{: f} {: f}".format(12.23, -12.23))
+
+
+# string padding with left alignment
+print("{:5}".format("cat"))
+
+# string padding with right alignment
+print("{:>5}".format("cat"))
+
+# string padding with center alignment
+print("{:^5}".format("cat"))
+
+# string padding with center alignment
+# and '*' padding character
+print("{:*^5}".format("cat"))
