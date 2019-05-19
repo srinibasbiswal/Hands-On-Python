8ca8be3ec62e4c3019ee7e32a909d76857aad17e
Srinibas Biswal <srinibasbiswal12@gmail.com>
2019-05-19 20:05:44 +0530

    Add Module files

---
+++ modules/myFile.py
@@ -0,0 +1,7 @@
+import myModule
+
+input = input("Enter your Name : ")
+myModule.printName(input)
+
+print("The value of var1 from the module is : ",myModule.var1)
+print("The value of var2 from the module is : ",myModule.var2)
---
+++ modules/myModule.py
@@ -0,0 +1,8 @@
+print("Module1 is imported")
+
+def printName(name):
+  print("The Name received from the main file is : ",name)
+
+var1 = 10
+var2 = "Python"
+
