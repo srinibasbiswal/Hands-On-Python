d515474eee7ba9184c69288c585104421ee69108
Srinibas Biswal <srinibasbiswal12@gmail.com>
2019-05-21 08:37:48 +0530

    userinput file

---
+++ usrinput.py
@@ -0,0 +1,15 @@
+
+#Take string type value
+inp = input("Enter the value : ")
+print("The user input is : ",inp)
+print (type(int(inp)))
+
+#Take int type value
+inp = int(input("Enter the value : "))
+print("The user input is : ",inp)
+print (type(int(inp)))
+
+#Available in python2 only
+# inp = raw_input("Enter the value : ")
+# print("The user input is : ",inp)
+# print (type(inp))
